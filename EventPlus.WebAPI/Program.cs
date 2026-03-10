2c98eba8843ff1677dd893ca38f2812ee49cca84
HENRIQUE DO NASCIMENTO <42916360832@info.local>
2026-03-10 16:48:43 -0300

    atividade

---
+++ EventPlus.WebAPI/Program.cs
@@ -0,0 +1,81 @@
+using EventPlus.WebAPI.BdContextEvent;
+using EventPlus.WebAPI.Interfaces;
+using EventPlus.WebAPI.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi;
+using System.ComponentModel;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<EventContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddScoped<ITipoEventoRepository, TipoEventoRepository>();
+
+// Adiciona Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "API de Eventos",
+        Description = "Aplicação para gerenciamento de eventos",
+        TermsOfService = new Uri("http://www.linkedin.com/in/marcaumdev"),
+        Contact = new OpenApiContact
+        {
+            Name = "Henrique",
+            Url = new Uri("https://www.linkedin.com/in/marcaumdev")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Exemplo de licensa",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Insira o token JWT: "
+
+    });
+
+    options.AddSecurityRequirement(document => new OpenApiSecurityRequirement
+    {
+        [new OpenApiSecuritySchemeReference("Bearer",document)] = Array.Empty<string>().ToList()
+       });
+});
+
+builder.Services.AddOpenApi();
+    
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger(options => { });
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ EventPlus.WebAPI/Repositories/InstituicaoRepository.cs
@@ -0,0 +1,80 @@
+﻿using EventPlus.WebAPI.BdContextEvent;
+using EventPlus.WebAPI.Interfaces;
+using EventPlus.WebAPI.Models;
+
+namespace EventPlus.WebAPI.Repositories;
+
+public class InstituicaoRepository :
+    IInstituicaoRepository
+{
+    private readonly EventContext _context;
+
+    public InstituicaoRepository (EventContext context)
+    {
+        _context = context;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="instituicao"></param>
+
+    public void Atualizar(Guid id, Instituicao instituicao)
+    {
+        var instituicaoBuscado = _context.Instituicaos.Find(id);
+        if(instituicaoBuscado != null)
+        {
+            instituicaoBuscado.NomeFantasia = instituicao.NomeFantasia;
+            _context.SaveChanges();
+
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+
+    public Instituicao BuscarPorId(Guid id)
+    {
+        return _context.Instituicaos.Find(id)!;
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="instituicao"></param>
+
+
+    public void Cadastrar(Instituicao instituicao)
+    {
+        _context.Instituicaos.Add(instituicao);
+        _context.SaveChanges();
+    }
+
+    public void Deletar(Guid id)
+    {
+        var instituicaoBuscado = _context.Instituicaos.Find(id);
+        if (instituicaoBuscado != null)
+        {
+            _context.Instituicaos.Remove(instituicaoBuscado);
+            _context.SaveChanges();
+
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    
+
+    public List<Instituicao> Listar()
+    {
+        return _context.Instituicaos
+            .OrderBy(instituicao => instituicao.NomeFantasia)
+            .ToList();
+    }
+}
